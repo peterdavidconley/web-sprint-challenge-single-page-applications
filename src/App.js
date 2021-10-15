e82fe6b78b4823fd489281042fc61cd255e1d878
Peter Conley <peterdavidconley@gmail.com>
2021-10-15 11:25:10 -0400

    created homepage component and rendered to the DOM by appending to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { Route, Link, Switch } from 'react-router-dom'
+import Homepage from './Homepage'
+
 
 const App = () => {
   return (
     <div>
-      
+      <Switch>
+        <Homepage />
+      </Switch>
     </div>
   );
 };
--- src/Homepage.js
+++ src/Homepage.js
@@ -14,40 +14,40 @@ function Homepage () {
         <div>
         <section id="pizza-header">
         <center>
-            <h3>YOUR FAVORITE FOOD, DELIVERED WHILE CODING</h3>
+            <h3>Your favorite food, delivered while coding.</h3>
             <button id='order-pizza' onClick={pizzaRoute}>Pizza?</button>
         </center>
         </section> 
         <section id="six-images" class="bgimg">
         <div>
-            <img src="https://media.istockphoto.com/photos/food-backgrounds-table-filled-with-large-variety-of-food-picture-id1155240408" alt="McDonald's photo"/>
+            <img src="https://via.placeholder.com/250x150" alt="McDonald's photo"/>
             <h3>McDonald's</h3>
                 <p> $ - American - Fast Food - Burgers</p>
                 <button>20-30 Min</button>
                 <button>$5.99 Delivery Fee</button>
         </div>
         <div>
-            <img src="https://media.istockphoto.com/photos/food-backgrounds-table-filled-with-large-variety-of-food-picture-id1155240408" alt="Sweetgreen photo"/>
+            <img src="https://via.placeholder.com/250x150" alt="Sweetgreen photo"/>
             <h3>Sweetgreen</h3>
                 <p> $ - Healthy - Salads</p>
                 <button>30-40 Min</button>
                 <button>$4.99 Delivery Fee</button>
         </div>
         <div>
-            <img src="https://media.istockphoto.com/photos/food-backgrounds-table-filled-with-large-variety-of-food-picture-id1155240408" alt="Starbuck's photo"/>
+            <img src="https://via.placeholder.com/250x150" alt="Starbuck's photo"/>
             <h3>Starbuck's</h3>
                 <p> $ - Cafe - Coffee & Tea - Breakfast</p>
                 <button>10-20 Min</button>
                 <button>$3.99 Delivery Fee</button>
         </div>
         <div>
-            <img src="https://media.istockphoto.com/photos/food-backgrounds-table-filled-with-large-variety-of-food-picture-id1155240408"/>
+            <img src="https://via.placeholder.com/250x150"/>
         </div>
         <div>
-            <img src="https://media.istockphoto.com/photos/food-backgrounds-table-filled-with-large-variety-of-food-picture-id1155240408"/>
+            <img src="https://via.placeholder.com/250x150"/>
         </div>
         <div>
-            <img src="https://media.istockphoto.com/photos/food-backgrounds-table-filled-with-large-variety-of-food-picture-id1155240408"/>
+            <img src="https://via.placeholder.com/250x150"/>
         </div>
         </section> 
         </div>

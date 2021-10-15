ce54888fee9e8727494746be29039f7581151b6e
Peter Conley <peterdavidconley@gmail.com>
2021-10-15 10:41:59 -0400

    created header and basic styling

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import React from "react";
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div>
+      
+    </div>
   );
 };
 export default App;
